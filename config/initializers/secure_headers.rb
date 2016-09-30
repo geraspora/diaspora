e53f4015fe831b5f0599ac23e125b72c128788b2
Dennis Schubert <mail@dennis-schubert.de>
2016-09-30 02:19:07 +0200

    Add CPS exceptions for deployment specific stuff

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,15 +6,15 @@ SecureHeaders::Configuration.default do |config|
     child_src:       %w('self' www.youtube.com w.soundcloud.com twitter.com platform.twitter.com syndication.twitter.com
                         player.vimeo.com www.mixcloud.com www.dailymotion.com media.ccc.de bandcamp.com
                         www.instagram.com),
-    connect_src:     %w('self' embedr.flickr.com geo.query.yahoo.com nominatim.openstreetmap.org api.github.com),
+    connect_src:     %w('self' *.geraspora.de embedr.flickr.com geo.query.yahoo.com nominatim.openstreetmap.org api.github.com),
     font_src:        %w('self'),
     form_action:     %w('self' platform.twitter.com syndication.twitter.com),
     frame_ancestors: %w('self'),
     img_src:         %w('self' data: *),
     media_src:       %w(https:),
-    script_src:      %w('self' 'unsafe-eval' platform.twitter.com cdn.syndication.twimg.com widgets.flickr.com
+    script_src:      %w('self' 'unsafe-eval' *.geraspora.de platform.twitter.com cdn.syndication.twimg.com widgets.flickr.com
                         embedr.flickr.com platform.instagram.com 'unsafe-inline'),
-    style_src:       %w('self' 'unsafe-inline' platform.twitter.com *.twimg.com)
+    style_src:       %w('self' 'unsafe-inline' *.geraspora.de platform.twitter.com *.twimg.com)
   }
 
   if AppConfig.environment.assets.host.present?
